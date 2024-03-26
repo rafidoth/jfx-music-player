4ee023b7c22f8c96832be34ec6e5d23e55c82aae
raphy <rafiulhasan803@gmail.com>
2024-03-26 22:58:16 +0600

    usrnamepasswordfield

--- src/main/java/com/mp/demo/Controllers/NewUserController.java
+++ src/main/java/com/mp/demo/Controllers/NewUserController.java
@@ -1,4 +1,26 @@
 package com.mp.demo.Controllers;
 
+import atlantafx.base.controls.PasswordTextField;
+import com.mp.demo.Model.UserModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class NewUserController {
+    UserModel newUser ;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordTextField passwordField;
+    @FXML
+    private Button createAccountBtn;
+
+    public void createAccount(){
+        String username = usernameField.getText();
+        String password = passwordField.getPassword();
+        System.out.println(username+ " "+ password);
+    }
+
+
 }
